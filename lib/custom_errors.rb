5ee2cb44b4a9f7dfa647729045f239d770b4074e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-04 02:42:13 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,11 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
-      if person.class != Person
+    if person.class != Person
+      begin
         raise PartnerError
-      else
-        person.partner = self
+      rescue PartnerError => error
+          puts error.message
       end
+    else
+      person.partner = self
+    end
   end
 
   class PartnerError < StandardError
