bb59e38d2d3c50acfef5ca61eb45728300370aa7
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-04 02:40:08 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,12 +10,14 @@ class Person
     person.partner = self
   end
 
+  class PartnerError < StandardError
+  end
+
 end
+
+
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
